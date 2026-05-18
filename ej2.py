8ab58818e6ed9a11412f88d818d0e355611dd3ee
Juan Pablo <juanpablo3705@gmail.com>
2026-05-18 18:41:37 -0400

    primer avance, faltan verificaciones y revisar logica

---
+++ ej2.py
@@ -0,0 +1,47 @@
+print("¡Bienvenido al sistema de gestión de espacios del Almacén Industrial!")
+
+capacidad_total = 60
+espacios_disponibles = 60
+
+while True:
+
+    print("=== MENÚ PRINCIPAL ===")
+    print("1. Espacios disponibles")
+    print("2. Ocupar espacio")
+    print("3. Liberar espacio")
+    print("4. Historial de ocupaciones")
+    print("5. Salir")
+
+    while True:
+        try:
+            opcion_menu = int(input("Ingrese una opción del menú: "))
+            break
+        except ValueError:
+            print("Error. Debe ingresar un número entero positivo del 1 al 5.")
+
+    match opcion_menu:
+
+        case 1:
+            print(f"Cantidad de espacios disponibles: {espacios_disponibles}.")
+
+        case 2:
+            espacios_ocupar = int(input("Ingrese la cantidad de espacios a ocupar: "))
+            if espacios_ocupar > 0 and espacios_ocupar <= espacios_disponibles:
+                espacios_disponibles = espacios_disponibles - espacios_ocupar
+            else:
+                print("Debe ingresar un valor mayor a cero y menor a la cantidad de espacios disponibles.")
+
+        case 3:
+            espacios_liberar = int(input("Ingrese la cantidad de espacios a liberar: "))
+            if espacios_liberar > 0 and espacios_liberar <= capacidad_total:
+                espacios_disponibles = espacios_disponibles + espacios_liberar
+
+        case 4:
+            print(f"Espacios disponibles: {espacios_disponibles}.")
+
+        case 5:
+            print("Gracias por utilizar nuestro software, hasta la próxima.")
+            break
+
+        case _:
+            print("Error. Debe ingresar un valor del 1 al 5.")
